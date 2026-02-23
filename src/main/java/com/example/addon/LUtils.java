d5c07368eda178dd6891935274621a782815ff66
LordLoading <lorenzdressel@gmail.com>
2026-02-23 01:47:36 +0100

    stealing from neki + unholy abominations of features

--- src/main/java/com/example/addon/LUtils.java
+++ src/main/java/com/example/addon/LUtils.java
@@ -2,7 +2,7 @@ package com.example.addon;
 
 import com.example.addon.commands.CommandExample;
 import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
+import com.example.addon.modules.Eloadra;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
@@ -13,17 +13,17 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
-public class AddonTemplate extends MeteorAddon {
+public class LUtils extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
+    public static final Category CATEGORY = new Category("LUtils");
     public static final HudGroup HUD_GROUP = new HudGroup("Example");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing LUtils");
 
         // Modules
-        Modules.get().add(new ModuleExample());
+        Modules.get().add(new Eloadra());
 
         // Commands
         Commands.add(new CommandExample());
