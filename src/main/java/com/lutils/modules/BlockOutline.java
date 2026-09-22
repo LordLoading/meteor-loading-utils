59529082b09bc1fa4e4b63ca6e8736fa7c00ed75
LordLoading <92874337+lordloading@users.noreply.github.com>
2026-09-22 22:55:34 +0200

    some fixes

--- src/main/java/com/lutils/modules/BlockOutline.java
+++ src/main/java/com/lutils/modules/BlockOutline.java
@@ -73,7 +73,7 @@ public class BlockOutline extends Module {
     private final Setting<SettingColor> breakProgressColor = sgGeneral.add(new ColorSetting.Builder()
         .name("break progress color")
         .description("The color of the outline when the block is being broken.")
-        .defaultValue(new SettingColor(255, 100, 100, 0))
+        .defaultValue(new SettingColor(255, 100, 100, 255))
         .visible(() -> breakProgress.get())
         .build()
     );
